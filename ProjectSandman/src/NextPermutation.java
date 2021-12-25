391c573402ea14610cb1cd31083e1d91d1de119e
Karthik Kolathumani <karthik_kr@hotmail.com>
2021-12-24 21:20:43 -0800

    Next Permutation

---
+++ ProjectSandman/src/NextPermutation.java
@@ -0,0 +1,50 @@
+public class NextPermutation {
+
+    // [1, 2, 3]
+    // [1, 3, 2]
+    //        ^
+    public static void nextPermutation(int[] nums){
+        int bckPtr = nums.length - 2;
+        int fwdPtr = 0;
+
+        while( bckPtr >= 0 && nums[bckPtr + 1] <= nums[bckPtr] ){
+            bckPtr--;
+        }
+
+        if(bckPtr >= 0){
+            int j = nums.length - 1;
+            while(nums[j] <= nums[bckPtr]){
+                j--;
+            }
+            swap(nums, bckPtr, j);
+        }
+
+        reverse(nums, bckPtr + 1);
+    }
+
+    public  static void swap(int nums[], int bckPtr, int j){
+        nums[bckPtr] = nums[bckPtr] ^ nums[j];
+        nums[j]      = nums[bckPtr] ^ nums[j];
+        nums[bckPtr] = nums[bckPtr] ^ nums[j];
+    }
+
+    public static void reverse(int nums[], int bckPtr){
+        int i = bckPtr, j = nums.length -1;
+
+        while(i < j){
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {1, 5, 8, 4, 7, 6, 5, 3, 1};
+        nextPermutation(nums);
+
+        for(Integer i : nums){
+            System.out.println(i);
+        }
+    }
+
+}
