f12c43e5c3277b30e7c9fe288bfa3eb9eb549d5e
Karthik Kolathumani <karthik_kr@hotmail.com>
2021-12-27 10:02:40 -0800

    LongestSubstring

--- ProjectSandman/src/LongestSubstringWithoutRepeatingCharacters.java
+++ ProjectSandman/src/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,2 +1,37 @@
+
+import java.util.HashMap;
 public class LongestSubstringWithoutRepeatingCharacters {
+
+    /**
+     * This one uses HashMap
+     * @param s
+     * @return
+     */
+    public static int lengthOfLongestSubstring(String s){
+        HashMap<Character, Integer> map = new HashMap<>(); // <Non Repeating, index>
+        int maxLen = 0;
+
+        int fwdPtr = 0;
+        int bckPtr = 0;
+
+        while(fwdPtr < s.length()){
+            char curr = s.charAt(fwdPtr);
+
+            if(map.containsKey(curr)){
+                map.remove(s.charAt(bckPtr));
+                bckPtr++;
+
+            }else{
+                map.put(curr, fwdPtr);
+                fwdPtr++;
+                maxLen = Math.max(maxLen, map.size());
+            }
+
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
+    }
 }
