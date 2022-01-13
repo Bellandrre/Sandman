5101fc0909086618082f91d6b8e4972e2f4b2355
Karthik Kolathumani <karthik_kr@hotmail.com>
2022-01-13 09:18:12 -0800

    Basic Tree class

---
+++ ProjectSandman/src/Trees/TreeNode.java
@@ -0,0 +1,21 @@
+package Trees;
+
+public class TreeNode {
+
+    TreeNode  left;
+    TreeNode right;
+    int value;
+
+    TreeNode(int value){
+        this.left = null;
+        this.right = null;
+        this.value = value;
+    }
+
+    public boolean addNode(TreeNode newNode){
+
+        while(newNode != null){
+            newNode = 
+        }
+    }
+}
